c893c1cafc2b2573d8f0e5ff3ee439e5a53c1e51
amal-prem <aldamal.prem@gmail.com>
2023-07-24 21:18:53 -0400

    fixes searchterm function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=d829e2d5";
+import "./App.css";
+
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
-
-  useEffect(() => {
-    searchMovies("Spiderman");
-  }, []);
 
   return (
     <div className="app">
@@ -21,13 +27,28 @@ const App = () => {
 
       <div className="search">
         <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for Movies"
-          value="Joker"
-          onChange={() => {}}
         />
-        <img src={SearchIcon} alt="search" onChange={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
-      <div className="container"></div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
